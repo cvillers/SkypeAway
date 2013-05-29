9efcdd391321131dbfe302c0a601e59bd40cbbb5
Cameron Villers <cameron@ville.rs>
2013-05-29 00:49:30 -0400

    Initial structure, test UI shell

---
+++ SkypeAway/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SkypeAway
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public static RoutedCommand StatusCommand = new RoutedCommand();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void StatusCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			MessageBox.Show("Selected " + e.Parameter.ToString());
+		}
+	}
+}
---
+++ SkypeAway/NativeMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SkypeAway.NativeMethods
+{
+	/// <summary>
+	/// Contains P/Invoke function definitions.
+	/// </summary>
+	public static class NativeMethods
+	{
+		[DllImport("user32.dll", SetLastError = true)]
+		static extern IntPtr FindWindow(string className, string windowTitle);
+
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
+	}
+}
